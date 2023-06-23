ffd2a42364f99488ba9f81d968605dbf453c11a8
Maria Zinkevich <maria.zinkevich@tangiblee.com>
2023-06-24 00:00:20 +0300

    fix: sku attribute in table row

--- ishop/src/components/TableRow.jsx
+++ ishop/src/components/TableRow.jsx
@@ -4,12 +4,11 @@ import PropTypes from 'prop-types';
 export const TableRow = ({product}) => {
 
   TableRow.propTypes = {
-    product: PropTypes.object.isRequired,
-    index: PropTypes.number.isRequired,
+    product: PropTypes.object.isRequired
   }
 
   return (
-    <tr key={product.sku}>
+    <tr sku={product.sku}>
         <td>{product.title}</td>
         <td>{product.price}</td>
         <td><a href={product.link}><img src={product.url} alt='img' width={100} height={100} /></a></td>
