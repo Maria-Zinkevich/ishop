dca49889a1d52de345e89b93fae06d354c7d87a3
Maria Zinkevich <maria.zinkevich@tangiblee.com>
2023-07-03 00:02:12 +0300

    fix: logic in sort and filter functions

--- filter/src/components/Filter.jsx
+++ filter/src/components/Filter.jsx
@@ -3,51 +3,53 @@ import PropTypes from 'prop-types';
 import { useState } from "react";
 
 export const Filter = ({ arrayToFilter }) => {
+
   Filter.propTypes = {
     arrayToFilter: PropTypes.arrayOf(PropTypes.string),
   }
 
   const [isChecked, setIsChecked] = useState(false);
   const [inputValue, setInputValue] = useState("");
-  const [sortedArray, setSortedArray] = useState(arrayToFilter);
+  const [processedArray, setProcessedArray] = useState(arrayToFilter);
 
-  function checkbox () {
+  function sort () {
+    setIsChecked(!isChecked);
     if (!isChecked) {
-      let arr = sortedArray.slice()
-      setSortedArray(arr.sort());
+      setProcessedArray([...processedArray].sort());
     } else {
-      setSortedArray(arrayToFilter);
+      let filteredArray = arrayToFilter.filter(element => processedArray.includes(element));
+      setProcessedArray(filteredArray);
+    }
+  }
+
+  function filter (event) {
+    setInputValue(event.target.value);
+    if (event.target.value.trim() === "") {
+      setProcessedArray(arrayToFilter);
+    } else {
+      let filteredArray = arrayToFilter.filter((item) =>{
+        return item.toLowerCase().includes(event.target.value.toLowerCase());
+      });
+      setProcessedArray(filteredArray);
     }
   }
 
   function reset() {
     setInputValue("");
     setIsChecked(false);
-    setSortedArray(arrayToFilter);
-  }
-
-  function change (event) {
-    setInputValue(event.target.value);
-    if (event.target.value.trim() === "") {
-      setSortedArray(arrayToFilter);
-    } else {
-      let sortedArrayFromInput = arrayToFilter.filter((item) =>{
-        return item.toLowerCase().includes(event.target.value.toLowerCase());
-      });
-      setSortedArray(sortedArrayFromInput);
-    }
+    setProcessedArray(arrayToFilter);
   }
 
   return (
     <>
       <form>
-        <input type="checkbox" value={inputValue} onClick={checkbox} />
-        <input type="text" onChange={change}/>
+        <input type="checkbox" checked={isChecked} onChange={sort} />
+        <input type="text" value={inputValue} onChange={filter}/>
         <input type="button" value="Reset" onClick={reset} />
       </form>
       <div id="scroll-wrap">
         <ul>
-          {sortedArray.map((word, index)=> {
+          {processedArray.map((word, index)=> {
             return <li key={index} >{word}</li>
           })}
         </ul>
@@ -55,65 +57,3 @@ export const Filter = ({ arrayToFilter }) => {
     </>
   )
 }
-// import React, { useState } from 'react';
-
-// export const Filter = ({ arrayToFilter }) => {
-//   const [searchText, setSearchText] = useState('');
-//   const [sortAlphabetically, setSortAlphabetically] = useState(false);
-//   const [filteredWords, setFilteredWords] = useState(arrayToFilter);
-
-//   const handleInputChange = (event) => {
-//     const text = event.target.value;
-//     setSearchText(text);
-
-//     if (text.trim() === '') {
-//       setFilteredWords(arrayToFilter);
-//     } else {
-//       const filtered = arrayToFilter.filter((word) =>
-//         word.toLowerCase().includes(text.toLowerCase())
-//       );
-//       setFilteredWords(filtered);
-//     }
-//   };
-
-//   const handleCheckboxChange = () => {
-//     setSortAlphabetically(!sortAlphabetically);
-//     if (!sortAlphabetically) {
-//       setFilteredWords([...filteredWords].sort());
-//     } else {
-//       setFilteredWords(arrayToFilter);
-//     }
-//   };
-
-//   const handleResetClick = () => {
-//     setSearchText('');
-//     setSortAlphabetically(false);
-//     setFilteredWords(arrayToFilter);
-//   };
-
-//   return (
-//     <div>
-//       <input
-//         type="text"
-//         value={searchText}
-//         onChange={handleInputChange}
-//         placeholder="Search..."
-//       />
-//       <label>
-//         <input
-//           type="checkbox"
-//           checked={sortAlphabetically}
-//           onChange={handleCheckboxChange}
-//         />
-//         Sort Alphabetically
-//       </label>
-//       <button onClick={handleResetClick}>Reset</button>
-
-//       <ul>
-//         {filteredWords.map((word) => (
-//           <li key={word}>{word}</li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// };
