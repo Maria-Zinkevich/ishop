706d26347640cfbbb74f6437796ccde60161278a
Maria Zinkevich <maria.zinkevich@tangiblee.com>
2023-07-23 13:33:25 +0300

    fix: br2jsx task

--- br2jsx/src/components/BR2JSX.jsx
+++ br2jsx/src/components/BR2JSX.jsx
@@ -1,18 +1,73 @@
+// import React, { Component } from 'react';
+
+// class BR2JSX extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       words: props.text
+//     };
+//   };
+//   render() {
+//     let wordsArray = this.state.words.split(/<br\s*\/?>/i)
+//     let wordsArrayWithBr = []
+//     wordsArray.forEach((element,index) => {
+//       if (index) {
+//         wordsArrayWithBr.push(<br key={index} />)
+  
+//       }
+//       wordsArrayWithBr.push(element)
+     
+//     });
+
+//     return (
+//       <div>
+//         {wordsArrayWithBr.map(elem => elem)}
+//       </div>
+//   )
+//   }
+// }
+
+// export default BR2JSX;
+
 import React, { Component } from 'react';
 
 class BR2JSX extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      wordsArray: props.text.replace(/<[^>]+>/g, ',').split(',')
+      words: props.text,
+      wordsArrayWithBr: []
     };
   };
+
+  componentDidMount() {
+    this.processedWords()
+  }
+  processedWords =() => {
+    let wordsArray = this.state.words.split(/<br\s*\/?>/i)
+    let resArr = []
+    wordsArray.forEach((element,index) => {
+      if (index) {
+        resArr.push(<br key={index} />)
+      }
+      resArr.push(element)
+      
+    });
+    this.setState({wordsArrayWithBr:resArr})
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.props.text !== prevProps.text) {
+      this.processedWords()
+    }
+  }
+
   render() {
     return (
       <div>
-          {this.state.wordsArray.map((word, index) => <p key={index}>{word}</p>)}
+        {this.state.wordsArrayWithBr.map(elem => elem)}
       </div>
-    );
+  )
   }
 }
 
