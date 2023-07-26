784e47b193d0b8a1b1705739a09ae45c709e0058
Maria Zinkevich <maria.zinkevich@tangiblee.com>
2023-07-27 00:53:59 +0300

    fix rainbowframe task

--- RainbowFrame/src/components/RainbowFrame.jsx
+++ RainbowFrame/src/components/RainbowFrame.jsx
@@ -25,7 +25,7 @@ class RainbowFrame extends Component {
     super(props);
     this.state = {
       colors: props.colors,
-      childToRender: this.props.children
+      childToRender: props.children
     };
   };
 
@@ -34,19 +34,13 @@ class RainbowFrame extends Component {
   }
 
   componentDidUpdate(prevProps, prevState) {
-    //корректна ли эта проверка?
-    //как проверить изменение слова из App.js 'hi'? 
     if (this.props.colors !== prevProps.colors) {
       this.processedChildToRender();
     }
   }
 
   processedChildToRender =() => {
-    //здесь если я не определю в updateChildToRender = this.state.childToRender у меня 
-    //появляются только рамки без текста, почему? ведь при первом рендере должен попасть
-    //первый childToRender: this.props.children, а это 'hi' из App.js
-    //let updateChildToRender;
-    let updateChildToRender = this.state.childToRender;
+    let updateChildToRender = this.props.children;
     this.state.colors.forEach((color) => {  
       updateChildToRender = <div style={{padding: "5px",border:`solid 5px ${color}`}}>{updateChildToRender}</div> 
       this.setState({childToRender: updateChildToRender})
